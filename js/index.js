ea7ce7cd5ffa6710fc8e74e075e646a2340d5b08
ZSGaby <135387096+ZSGaby@users.noreply.github.com>
2023-07-28 16:49:03 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
-function enviarTexto(){
-    var texto= document.getElementById("inputText2").value;
-    document.getElementById("inputText").value=texto;
+window.onload = function(){
+    const mensajesChat = document.getElementById("area-Mensaje");
+    const mensajeInput = document.getElementById("texto");
+    const botonEnviar = document.getElementById("boton-enviar");
+
+    function mostrarMensajesChat(mensaje, remitente, esUsuario){
+        const nuevoMensaje = document.createElement('div');
+        nuevoMensaje.classList.add('area-Mensaje');
+        if(esUsuario){
+            nuevoMensaje.classList.add('Usuario');
+        }
+        else{
+            nuevoMensaje.classList.add('Random');
+        }
+        nuevoMensaje.textContent = `${remitente}: ${mensaje}`;
+        mensajesChat.appendChild(nuevoMensaje);
+    }
+    //funcion donde se hacen las respuestas de manera aleatoria
+    function generadorRandomRespuesta(){
+        const respuestas = [
+            "Hola","¿Cómo puedo ayudarte?","Dime más",
+            "Eso suena bastante bien", "Dejame pensar acerca de ello",
+            "Lo siento no entiendo, ¿podrías volver a escribirlo?",
+            "Esa es una buena pregunta","Esa es una interesante pregunta"
+        ];
+        const randomIndice = Math.floor(Math.random() * respuestas.length);
+        return respuestas[randomIndice];
+    }
+
+    //funcion para enviar mensajes en el chat
+    function enviarMensaje(){
+        const mensaje = mensajeInput.value;
+
+        if(mensaje.trim() !== ''){
+            mostrarMensajesChat(mensaje, 'Tú');
+            mensajeInput.value='';
+        }
+        setTimeout(function(){
+            const botResponde = generadorRandomRespuesta();
+            mostrarMensajesChat(botResponde, 'Chatbot');
+        },1000);
+    }
+    botonEnviar.addEventListener('click',enviarMensaje);
 }
